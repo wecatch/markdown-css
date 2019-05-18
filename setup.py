53d9d0a08f60b8cf5f91c018e0bea7710b4cd38c
zhaoyongqiang <zhyq0826@gmail.com>
2019-05-18 23:57:01 +0800

    add dep

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 ]
 
-for k in ['docopt', 'cssutils', 'pyquery']:
+for k in ['docopt', 'cssutils', 'pyquery', 'Pygments==2.4.0']:
     try:
         __import__(k)
     except ImportError:
